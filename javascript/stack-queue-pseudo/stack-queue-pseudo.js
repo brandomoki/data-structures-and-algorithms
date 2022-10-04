7d8117bb070371748953bbb52855fbdcb187d5e3
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-10-04 18:22:22 +0000

    refactored code

--- javascript/stack-queue-pseudo/stack-queue-pseudo.js
+++ javascript/stack-queue-pseudo/stack-queue-pseudo.js
@@ -1,23 +1,32 @@
-'use strict';
+class PseudoQueue {
 
-
-
-class PsuedoQueue() {
-  constructor(){
+  constructor() {
     this.pushStack = [];
     this.popStack = [];
   }
 
-  pushStack(value) {
+  push(value) {
     this.pushStack.push(value);
   }
 
-  popStack() {
+  pop() {
     if(!this.popStack.length) {
-      while(this.pushStack.length)
+      while(this.pushStack.length) {
         this.popStack.push(this.pushStack.pop());
       }
     }
     return this.popStack.pop();
-
+  }
 }
+
+let queue = new PseudoQueue;
+
+queue.push(1);
+queue.push(2);
+queue.push(3);
+
+console.log(queue);
+
+let value = queue.pop();
+
+console.log(value);
