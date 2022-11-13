565ce0a9d1bfba8cfcd6f776c342d070f9b75eca
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-11-13 21:38:56 +0000

    completed challenge

--- javascript/graph-breadth-first/graph-breadth-first.js
+++ javascript/graph-breadth-first/graph-breadth-first.js
@@ -1,7 +1,4 @@
 
-
-
-/* A function that takes in a graph and a startNode. */
 function breadthFirst(graph, startNode) {
   const queue = [];
   const visitedNodes = new Set();
@@ -27,21 +24,4 @@ function breadthFirst(graph, startNode) {
 
 module.exports = breadthFirst;
 
-// Tests
 
-// /* Creating a graph with the nodes and their neighbors. */
-// const graph = {
-//   a: ['b', 'c', 'd'],
-//   b: ['a', 'd'],
-//   c: ['a', 'e'],
-//   d: ['a', 'b'],
-//   e: ['c'],
-//   f: ['g'],
-//   g: ['f'],
-// };
-
-// let nodes = breadthFirst(graph, 'a');
-// console.log(nodes); // Set { 'a', 'b', 'c', 'd', 'e' }
-
-// nodes = breadthFirst(graph, 'd');
-// console.log(nodes); // Set { 'd', 'a', 'b', 'c', 'e' }
